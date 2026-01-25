9cb0121cbdb7635248c1ce0e755c58daf8fbf67d
Vulpixass <maxderfuchs165@gmail.de>
2026-01-25 11:31:49 +0100

    Initial Commit

---
+++ src/main/java/net/vulpixass/soulspire/SoulspireClient.java
@@ -0,0 +1,10 @@
+package net.vulpixass.soulspire;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SoulspireClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/vulpixass/soulspire/SoulspireDataGenerator.java
@@ -0,0 +1,11 @@
+package net.vulpixass.soulspire;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SoulspireDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
